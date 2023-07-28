0cc4d8776e47637b1221e6103f05295039ddbea0
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-07-28 16:18:48 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select'
 import 'slim-select/dist/slimselect.css';
 
-//  new SlimSelect({
-//   select: '.breed-select'
-//  })
 
 
 const selector = document.querySelector('.breed-select');
@@ -18,8 +15,10 @@ loader.classList.replace('loader', 'is-hidden');
 error.classList.add('is-hidden');
 divCatInfo.classList.add('is-hidden');
 
+selector.addEventListener('change', onSelectBreedCat);
 
-fetchBreeds().then(console.log) 
+
+//fetchBreeds().then(console.log) 
 let arrBreedsId = [];
 fetchBreeds()
 .then(data => {
@@ -31,9 +30,41 @@ fetchBreeds()
         data: arrBreedsId
     });
 }).catch(onFetchError);
-console.log(arrBreedsId)
+//console.log(arrBreedsId)
+
+function onSelectBreedCat(evt) {
+    evt.preventDefault();
+    loader.classList.replace('is-hidden', 'loader');
+    selector.classList.add('is-hidden');
+    divCatInfo.classList.add('is-hidden');
+    //console.log(evt.currentTarget.value)
+    const breedId = evt.currentTarget.value;
+    fetchCatByBreed(breedId)
     
+        .then(data => {
+         loader.classList.replace('loader', 'is-hidden');
+        selector.classList.remove('is-hidden');
+        const { url, breeds } = data[0];
+        const catInfo = `<div class="box-img">
+        <img src="${url}" alt="${breeds[0].name}" width="300"/>
+        </div>
+        <div class="box">
+        <h1>${breeds[0].name}</h1>
+        <p>${breeds[0].description}</p>
+        <p><b>Temperament:</b> ${breeds[0].temperament}
+        </p>
+        </div>`
+        divCatInfo.classList.remove('is-hidden');
+        divCatInfo.innerHTML = catInfo;
+        
+    })
+    .catch(onFetchError);
+};
+
+
 function onFetchError(error) {
+ selector.classList.remove('is-hidden');
+loader.classList.replace('loader', 'is-hidden');
     Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
         position: 'center-center',
         timeout: 5000,
